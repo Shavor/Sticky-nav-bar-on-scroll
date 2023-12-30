5d349218eb66e90ffc8e665651a01a5fb2dba5c1
shavor <shavor80@yandex.ru>
2023-12-30 17:19:55 +0300

    scroll to top

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ window.addEventListener('scroll', ()=> {
   const header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+// SCROLL TO TOP
+
+const scrollBtn = document.querySelector('.top');
+const rootEl = document.documentElement;
+
+document.addEventListener('scroll', showBtn);
+scrollBtn.addEventListener('click', scrollToTop);
+
+function showBtn(){
+  const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
+  if(rootEl.scrollTop / scrollTotal > 0.3) {
+    scrollBtn.classList.add('show-btn');
+  } else {
+    scrollBtn.classList.remove('show-btn');
+  }
+}
+
+function scrollToTop(){
+  rootEl.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
